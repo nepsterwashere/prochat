2ac836f82e655f0f72c57d075f32ba9ce56ee01a
Jan Völker <jan-voelker@t-online.de>
2020-10-16 21:26:15 +0200

    update(client): added sass & removed boilerplate

--- prochat-client/src/index.jsx
+++ prochat-client/src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.scss';
+
+const App = () => (
+  <div>Hello ProChat-App</div>
+)
 
 ReactDOM.render(
   <React.StrictMode>
