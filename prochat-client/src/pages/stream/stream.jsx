54ff254ce3005a4cab3d69c6ed75b7c2569d9ce0
iamnepster <jan-voelker@t-online.de>
2021-04-20 14:46:48 +0200

    update(test): test

--- prochat-client/src/pages/stream/stream.jsx
+++ prochat-client/src/pages/stream/stream.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Chat } from '../../container/chat/chat'
-import VideoJS from '../../container/video/videojs'
 import './stream.scss'
 
 export default function Stream() {
@@ -8,7 +7,23 @@ export default function Stream() {
         <div className="pro-chat">
             <div className="pro-chat--video">
                 <h1 className="pro-chat--video-heading">{`${process.env.REACT_APP_VIDEO_HEADING || 'Live-Stream'}`}</h1>
-                <VideoJS />
+                <video
+                    id="my-video"
+                    fluid="true"
+                    class="video-js"
+                    controls
+                    preload="auto"
+                    data-setup="{}"
+                >
+                    <source 
+                        src={`${process.env.REACT_APP_VIDEO_URL}`} 
+                        type={`${process.env.REACT_APP_VIDEO_TYPE}`} 
+                    />
+                    <p class="vjs-no-js">
+                        To view this video please enable JavaScript, and consider upgrading to a
+                        web browser that supports HTML5 video
+                    </p>
+                </video>
             </div>
             <div className="pro-chat--chat">
                 <Chat />
