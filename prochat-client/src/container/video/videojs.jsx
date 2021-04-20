f7722e9678450e8773c9e8694c240e6571e4abfc
iamnepster <jan-voelker@t-online.de>
2021-04-20 09:34:40 +0200

    feat(videojs): component & hook added

--- prochat-client/src/container/video/videojs.jsx
+++ prochat-client/src/container/video/videojs.jsx
@@ -1,43 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import videojs from 'video.js'
+import React from 'react'
+import usePlayer from '../../hooks/player.hook'
 import './videojs.scss'
 
 export default function VideoJS() {
-    const [player, setPlayer] = useState()
-    const videoNode = ''
-    const videoJsOptions = {
-        autoplay: true,
-        controls: true,
-        sources: [{
-            src: `${process.env.REACT_APP_VIDEO_URL}`,
-            type: `${process.env.REACT_APP_VIDEO_TYPE}`
-        }]
-    }
+    const playerRef = usePlayer({
+        src: `${process.env.REACT_APP_VIDEO_URL}`, 
+        controls: true, 
+        autoplay: false
+    });
 
-    // instantiate Video.js
-    useEffect(() => {
-        setPlayer(videojs(
-            videoNode,
-            {...videoJsOptions},
-            function onPlayerReady() {
-                console.log('onPlayerReady', this)
-            })
-        )
-
-        // destroy player on unmount
-        return function cleanup() {
-            if (player) {
-                player.dispose()
-            }
-        };
-    }, [])
-
-    // wrap the player in a div with a `data-vjs-player` attribute
-    // so videojs won't create additional wrapper in the DOM
-    // see https://github.com/videojs/video.js/pull/3856
     return (
         <div data-vjs-player>
-            <video ref={node => this.videoNode = node} className="video-js"></video>
+            <video ref={playerRef} className="video-js"></video>
         </div>
     )
 }
--- prochat-client/src/pages/stream/stream.jsx
+++ prochat-client/src/pages/stream/stream.jsx
@@ -1,19 +1,17 @@
-import { Grid } from '@material-ui/core'
 import React from 'react'
 import { Chat } from '../../container/chat/chat'
 import VideoJS from '../../container/video/videojs'
+import './stream.scss'
 
 export default function Stream() {
     return (
-        <div>
-            <Grid>
-                <Grid>
-                    <VideoJS />
-                </Grid>
-                <Grid>
-                    <Chat />
-                </Grid>
-            </Grid>
+        <div className="pro-chat">
+            <div className="pro-chat--video">
+                <VideoJS />
+            </div>
+            <div className="pro-chat--chat">
+                <Chat />
+            </div>
         </div>
     )
 }
