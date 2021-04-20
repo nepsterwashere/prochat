8a9f050e4964ed9176df767f5d27e3bcbb623ed6
iamnepster <jan-voelker@t-online.de>
2021-04-20 10:07:23 +0200

    feat(ui): layout

--- prochat-client/src/pages/stream/stream.jsx
+++ prochat-client/src/pages/stream/stream.jsx
@@ -7,6 +7,7 @@ export default function Stream() {
     return (
         <div className="pro-chat">
             <div className="pro-chat--video">
+                <h1 className="pro-chat--video-heading">{`${process.env.REACT_APP_VIDEO_HEADING || 'Live-Stream'}`}</h1>
                 <VideoJS />
             </div>
             <div className="pro-chat--chat">
